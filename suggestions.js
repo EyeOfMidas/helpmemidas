a674e31f0f146ac537e8f10cf15718f7ba4372b7
Justin Gilman <justin@cirle.com>
2015-03-09 14:06:53 -0400

    initial commit

---
+++ suggestions.js
@@ -0,0 +1,25 @@
+var suggestions = [
+"Did you try rebooting?",
+"Google it!",
+"I'd suggest you read Clean Code again.",
+"Does it follow the Single Responsibility Principle?",
+"Is it open/closed? Open for extension, but closed for modification.",
+"Don't expose your privates!",
+"Keep it simple.",
+"If it ain't broke, don't fix it.",
+"If you're refactoring, you need to wear the refactoring hat, not the new features hat.",
+"Composition over inheritance.",
+"Did you find any results on Google?",
+"Does it follow the interface segregation principle?",
+"Dependency inversion: Depend on abstractions, not on concretions. (maybe you need a common interface there?)",
+"Take a step back and tell me what the <em>intention</em> of this is.",
+"I bet the man page would have some useful information. <code>man [command]</code>",
+"There isn't always a <em>best</em>. Use the right tool for the right job.",
+"Do one simple, easy-to-understand thing. Make it work. Then add to it.",
+"All of these features are nice, but we should really focus on the minimum viable product.",
+"You use regular expressions on string manipulation! It's super effective!",
+"To change a file to be executable, you'll have to <code>ch</code>ange it's <code>mod</code>ifiers. ;)"
+
+
+
+];
